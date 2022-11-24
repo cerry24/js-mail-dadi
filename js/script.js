80016291a1c2ebb6c9065037dd2302996e907c54
cerry24 <samuelecerretti@gmail.com>
2022-11-24 18:08:01 +0100

    implement mail verifier js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 // gioco dei dadi
 
-const buttonStart = document.getElementById ('btn-gioca');
+const buttonStart = document.getElementById ('btn-play');
 
 buttonStart.addEventListener ('click', function() {
+
     const userNumber = Math.floor (Math.random() * 6 + 1);
     const npcNumber = Math.floor (Math.random() * 6 + 1);
 
@@ -13,5 +14,40 @@ buttonStart.addEventListener ('click', function() {
 
     } else {
         document.getElementById('risultato').innerHTML = `Il tuo numero &egrave; ${userNumber} il mio &egrave; ${npcNumber} quindi &egrave; un pareggio`;
+    }
+} )
+
+
+
+
+// verifica email
+
+const mailList = [
+    'giggino@gmail.com',
+    'carletto45@gmail.com',
+    'soincredibbbile@libero.it',
+    'bomber8789@hotmail.net',
+    'nonhofantasiaperinventareindirizziemail@gmail.com'
+]
+
+const buttonVerify = document.getElementById ('btn-verify');
+
+buttonVerify.addEventListener ('click', function() {
+
+    const userMail = document.getElementById ('mail').value;
+
+    let found = false;
+
+    for ( i = 0; i < mailList.length; i++) {
+
+        if ( userMail == mailList[i] ) {
+            found = true;
+        }
+    }
+
+    if ( found ) {
+        document.getElementById('output').innerHTML = 'L\'indirizzo email immesso corrisponde';
+    } else {
+        document.getElementById('output').innerHTML = 'L\'indirizzo email immesso non corrisponde';
     }
 } )
